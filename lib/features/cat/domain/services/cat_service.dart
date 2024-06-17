726bcbdc71e69c3dbe5a7141f21145ff51eb9abe
Carlos Android Studio <carlos.sanchez.a0@gmail.com>
2024-06-17 01:53:58 -0500

    remove any

--- lib/features/cat/domain/services/cat_service.dart
+++
@@ -1,7 +0,0 @@
-import 'package:fpdart/fpdart.dart';
-import 'package:pruebatec/core/error/failure.dart';
-import 'package:pruebatec/features/cat/domain/entities/cat.dart';
-
-abstract interface class CatService {
-  Future<List<Cat>> findAll();
-}
